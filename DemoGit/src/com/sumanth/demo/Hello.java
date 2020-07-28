a6d0b0d271b0c265e9078231737a2e59a959c9ba
sumanth pippari <sumanth@gmail.com>
2020-07-28 13:05:44 +0530

    hello.java

--- DemoGit/src/com/sumanth/demo/Hello.java
+++ DemoGit/src/com/sumanth/demo/Hello.java
@@ -3,8 +3,9 @@ package com.sumanth.demo;
 
 public class Hello {
 
-	public void hello() {
-		System.out.println("hello");
+	public static void main(String[] args) {
+		Date u = new Date();
+		System.out.println(u.getSysDate());
+	}
 
 	}
-}
